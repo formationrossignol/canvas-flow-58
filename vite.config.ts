8ef230b73cb0329c4575850df8b6890965a967b2
Loïc <92857075+formationrossignol@users.noreply.github.com>
2025-10-06 15:24:27 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig({
-  base: '/portal/',
+  base: '/kanban/',
   plugins: [react()],
   server: {
     port: 8080,
